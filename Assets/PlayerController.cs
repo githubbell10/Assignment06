e2b9bcd287d17a4093762d774aa5cddb9e699708
githubbell10 <52224530+githubbell10@users.noreply.github.com>
2019-11-10 00:29:30 -0600

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Camera cam;
+    public NavMeshAgent agent;
+    public ThirdPersonCharacter character;
+
+    void Start()
+    {
+        agent.updateRotation = false;
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // MOVE OUR AGENT
+                agent.SetDestination(hit.point);
+            }
+        }
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            character.Move(Vector3.zero, false, false);
+        }
+    }
+
+}
---
+++ Assets/targetScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class targetScript : MonoBehaviour
+{
+    public GameObject player;
+    public static int targetsDestroyed = 0;
+    public Text scoreText;
+    public Text JumpScore;
+    public GameObject targetScore;
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        scoreText.text = "";
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            targetsDestroyed += 1;
+            scoreText.text = "Tagets destroyed: " + targetsDestroyed;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
